85de09828368a2ecb4c63df961adf7717a476871
qoxogus <a01034367055@gmail.com>
2024-07-07 03:07:12 +0900

    feat: 패키지 분리

--- src/main/java/com/spring/batch/config/batch/job/next/StepNextJobConfiguration.java
+++ src/main/java/com/spring/batch/config/batch/job/next/StepNextJobConfiguration.java
@@ -1,4 +1,4 @@
-package com.spring.batch.config.batch.job;
+package com.spring.batch.config.batch.job.next;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
--- src/main/java/com/spring/batch/config/batch/job/simple/SimpleBatchJobConfiguration.java
+++ src/main/java/com/spring/batch/config/batch/job/simple/SimpleBatchJobConfiguration.java
@@ -1,4 +1,4 @@
-package com.spring.batch.config.batch.job;
+package com.spring.batch.config.batch.job.simple;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
